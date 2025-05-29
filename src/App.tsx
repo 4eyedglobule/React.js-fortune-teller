9188607a147d766397f8ddd5fd937aa2a3e1f5d0
Jon Arbuckle <johnderugo@gmail.com>
2025-05-29 17:18:14 -0400

    Added image, removed alert, inserted font, updated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,85 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Button from "./components/Button";
-import Alert from './components/Alert';
 import "./App.css";
+import fortune from "./assets/fortune.jpg";
 
 function App() {
-  const [fortuneTold, setfortuneTold] = useState(false)
-  const [randomFortune, setRandomFortune] = useState("")
-  const [fortuneType,setFortuneType] = useState("")
-  const [fortuneColour, setFortuneColour] = useState("")
-  
+  const [fortuneTold, setfortuneTold] = useState(false);
+  const [randomFortune, setRandomFortune] = useState("");
+  const [fortuneType, setFortuneType] = useState("");
+  const [fortuneClass, setFortuneClass] = useState("");
+
   const fortuneList = [
-    ["Life will become hard soon.","Unfortunate fortune!!!","danger"],
-    ["It is best to be prepared.","Unfortunate fortune!!!","danger"],
-    ["Be careful not to overstep.","Unfortunate fortune!!!","danger"],
-    ["Pay attention to the people around you.","Unfortunate fortune!!!","danger"],
-    ["Things will likely stay the same.","Common fortune!!!","warning"],
-    ["Growth is slow. Be patient.","Common fortune!!!","warning"],
-    ["Look for quality, not quantity.","Common fortune!!!","warning"],
-    ["It does not matter the speed you go so long as you don't stop.","Common fortune!!!","warning"],
-    ["You have something to look forward to.","Rare fortune!!!","info"],
-    ["Soon, you will notice how many things in your life you are grateful for.","Rare fortune!!!","info"],
-    ["Something nice will occur to you unexpectedly.","Rare fortune!!!","info"],
-    ["Success is written in your destiny.","Auspicious fortune!!!","primary"],
-    ["Your life will be fulfilling and satisfying.","Auspicious fortune!!!","primary"],
-  ]
+    ["Life will become hard soon.", "Unfortunate fortune", "unfortunate"],
+    ["It is best to be prepared.", "Unfortunate fortune", "unfortunate"],
+    ["Be careful not to overstep.", "Unfortunate fortune", "unfortunate"],
+    [
+      "Pay attention to the people around you.",
+      "Unfortunate fortune",
+      "unfortunate",
+    ],
+    ["Things will likely stay the same.", "Common fortune", "common"],
+    ["Growth is slow. Be patient.", "Common fortune", "common"],
+    ["Look for quality, not quantity.", "Common fortune", "common"],
+    [
+      "It does not matter the speed you go so long as you don't stop.",
+      "Common fortune",
+      "common",
+    ],
+    ["You have something to look forward to.", "Rare fortune", "rare"],
+    [
+      "Soon, you will notice how many things in your life you are grateful for.",
+      "Rare fortune",
+      "rare",
+    ],
+    ["Something nice will occur to you unexpectedly.", "Rare fortune", "rare"],
+    ["Success is written in your destiny.", "Auspicious fortune", "auspicious"],
+    [
+      "Your life will be fulfilling and satisfying.",
+      "Auspicious fortune",
+      "auspicious",
+    ],
+  ];
 
   function decideFortune() {
-    setfortuneTold(true)
+    setfortuneTold(true);
     const min = 0;
     const max = Object.keys(fortuneList).length;
-    const rand = Math.round(min + Math.random() * (max - min));
-    console.log(rand);
-    console.log("Shit");
+    const rand = Math.floor(Math.random() * fortuneList.length);
     setRandomFortune(fortuneList[rand][0]);
-    setFortuneType(fortuneList[rand][1])
-    setFortuneColour(fortuneList[rand][2]);
+    setFortuneType(fortuneList[rand][1]);
+    setFortuneClass(fortuneList[rand][2]);
   }
 
-  document.body.style = 'background: lightGrey;';
+  document.body.style.background = "linear-gradient(45deg, #ed3512, #ffcc00)";
+  document.body.style.backgroundSize = "cover"; // or "100% 100%"
+  document.body.style.height = "100vh"; 
   return (
-    <div className="d-grid col-7 mx-auto " >
-      { !fortuneTold &&
-      <h1 className='head'>Fortune Teller 5000</h1>}
-      { !fortuneTold &&
-      <Button onClick={() => decideFortune()}>
-        Pick my Fortune
-      </Button>
-      }
-      { fortuneTold &&
-      <h1 className='fortune'>{randomFortune}</h1>
-      }
-      { fortuneTold &&
-      <Alert colour= {fortuneColour}>
-        {fortuneType}
-      </Alert>
-      }
-      { fortuneTold &&
-      <Button onClick={() => setfortuneTold(false)}>
-        Get another fortune
-      </Button>
-      }
+    <div className="back">
+      {!fortuneTold && <h1 className="head">Fortune Teller</h1>}
+      {!fortuneTold && (
+        <img
+          src={fortune}
+          alt="Fortune"
+          style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+        />
+      )}
+      {!fortuneTold && (
+        <div style={{ width: "300px", marginTop: "20px", marginLeft: 'auto',marginRight: 'auto',}}>
+          <Button onClick={() => decideFortune()}>Pick my Fortune</Button>
+        </div>
+      )}
+      {fortuneTold && <h1 className="fortune">{randomFortune}</h1>}
+      {fortuneTold && <h1 className={fortuneClass}>[{fortuneType}]</h1>}
+      {fortuneTold && (
+        <div style={{ width: "300px", marginTop: "20px", marginLeft: 'auto',marginRight: 'auto',}}>
+          <Button onClick={() => setfortuneTold(false)}>
+            Get another fortune
+          </Button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,19 @@
-
 interface Props {
-    children: string;
-    onClick: () => void;
+  children: string;
+  onClick: () => void;
 }
 
-const Button = ({ children, onClick }:Props) => {
+const Button = ({ children, onClick }: Props) => {
   return (
-    <button type="button" className={"btn text-nowrap btn-lg btn-outline-dark position-absolute top-50 start-50 translate-middle"} onClick={onClick}>
-      { children }
+    <button
+      type="button"
+      style={{ fontFamily: "Raleway", fontSize: "30px" }}
+      className={
+        "btn d-flex btn-lg btn-outline-dark justify-content-center font-family-Raleway"
+      }
+      onClick={onClick}
+    >
+      {children}
     </button>
   );
 };
